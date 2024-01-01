f9a2f5d04eaeca238bf72d7ea798c6f3ea608d3c
Andrea Idini <andrea.idini@matfys.lth.se>
2024-01-02 00:03:15 +0100

    Build Solar Panel implemented

--- Station.gd
+++ Station.gd
@@ -12,9 +12,11 @@ var battery_cap                 # KWh
 var power                       # KW
 var pow_consumption
 var solar_power_installed       # kW
-var modules_upkeep               # KW
+var modules_upkeep              # KW
+var power_construction=0.       # KW
 
 var matter_transf = 0.
+var matter_change
 var humans_cap
 
 var humans_matter_upkeep = 10   # Kg
@@ -26,71 +28,71 @@ var humans_rent = 20            # cred
 const STARTING = {
 	"name": "Starting Module",
 	"description": "The starting module is the core of your space station, built with the best materials designed to stand the test of time and already equipped with everything necessary to host humans and keep them warm and fed. It has beds for 3 humans, but not enough energy. Careful!",
-	"power": 15,                # KW
-	"battery": 50,              # KWh
-	"living_space": 3,          # Capacity for Humans
-	"upkeep_power": 3,         # KW
-	"upkeep_matter": 0,         # Kg/d
-	"build_matter": 5,           # ton
-	"build_energy": 100,           # KWh
+	"power": 15.,                # KW
+	"battery": 50.,              # KWh
+	"living_space": 3.,          # Capacity for Humans
+	"upkeep_power": 3.,         # KW
+	"upkeep_matter": 0.,         # Kg/d
+	"build_matter": 5.,           # ton
+	"build_energy": 100.,           # KWh
 	"build_credits": 3000,
-	"build_time": 30
+	"build_time": 30.
 }
 
 const SOLAR = {
 	"name": "Solar Array Wing", # Specs from ISS
 	"description": "The solar array wing is a module of solar panels, that can be extended and orientated to face the Sun as efficiently as possible and collect precious energy. Some battery included.",
-	"power": 30,                # KW
-	"battery": 5,               # KWh
-	"living_space": 0,          # Capacity for Humans
-	"upkeep_power": 0,         # KW
-	"upkeep_matter": 0,         # Kg/d
-	"build_matter": 1,           # ton
-	"build_energy": 20,          # KWh
+	"power": 30.,                # KW
+	"battery": 5.,               # KWh
+	"living_space": 0.,          # Capacity for Humans
+	"upkeep_power": 0.,         # KW
+	"upkeep_matter": 0.,         # Kg/d
+	"build_matter": 1.,           # ton
+	"build_energy": 20.,          # KWh
 	"build_credits": 500,
-	"build_time": 3
+	"build_time": 3.
 }
 
 const BATTERY = {
 	"name": "Battery Storage",  # 
 	"description": "The battery storage module will provide all your energy storage needs at the most convenient price, it needs some power and batteries need to be replaced from time to time.",
 	"power": 0,                 # KW
-	"battery": 50,              # KWh
-	"living_space": 0,          # Capacity for Humans
+	"battery": 50.,              # KWh
+	"living_space": 0.,          # Capacity for Humans
 	"upkeep_power": 0.1,       # KW
-	"upkeep_matter": 1,         # Kg/d
-	"build_matter": 1,           # ton
-	"build_energy": 30,          # KWh
+	"upkeep_matter": 1.,         # Kg/d
+	"build_matter": 1.,           # ton
+	"build_energy": 30.,          # KWh
 	"build_credits": 500,
-	"build_time": 5
+	"build_time": 5.
 }
 
 const BASIC_LIVING = {
 	"name": "Basic Living Facilities",
 	"description": "Living quarters for 5 humans, with some basic amenities but needs energy and material upkeep.",
-	"power": 0,                 # KW
-	"battery": 20,              # KWh
-	"living_space": 5,          # Capacity for Humans
-	"upkeep_power": 3,         # KW
-	"upkeep_matter": 5,         # Kg/d
-	"build_matter": 5,           # ton
-	"build_energy": 50,          # KWh
+	"power": 0.,                 # KW
+	"battery": 20.,              # KWh
+	"living_space": 5.,          # Capacity for Humans
+	"upkeep_power": 3.,         # KW
+	"upkeep_matter": 5.,         # Kg/d
+	"build_matter": 5.,           # ton
+	"build_energy": 50.,          # KWh
 	"build_credits": 2000,
-	"build_time": 10
+	"build_time": 10.
 }
 
 const DRONE = {
 	"name": "Mining Drone Bay",
 	"description": "Living quarters for 5 humans, with some basic amenities but needs energy and material upkeep.",
-	"power": 0,                 # KW
-	"battery": 20,              # KWh
-	"living_space": 5,          # Capacity for Humans
-	"upkeep_power": 3,         # KW
-	"upkeep_matter": 5,         # Kg/d
-	"build_matter": 5,           # ton
-	"build_energy": 50,          # KWh
+	"power": 0.,                 # KW
+	"battery": 20.,              # KWh
+	"living_space": 5.,          # Capacity for Humans
+	"upkeep_power": 3.,         # KW
+	"upkeep_matter": 5.,         # Kg/d
+	"build_matter": 5.,           # ton
+	"build_energy": 50.,          # KWh
 	"build_credits": 2000,
-	"build_time": 10
+	"build_time": 10.
 }
 
 var Modules = [STARTING]
@@ -104,35 +106,47 @@ var Modules = [STARTING]
 @onready var count_humans = %CountHumans
 @onready var count_consumption = %CountConsumption
 
+@onready var container_solar_panel = %ContainerSolarPanel
+@onready var label_experiments = %LabelExperiments
+@onready var label_drones = %LabelDrones
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("station ready", Modules)
 	station_properties()
 	print(battery_cap, " ",power)
 	
+	container_solar_panel.show()
+	container_solar_panel.get_child(0).text = "0"
+	
+	label_experiments.hide()
+	label_experiments.get_child(0).text = "0"
+	label_drones.hide()
+	label_drones.get_child(0).text = "0"
+
 	#Modules.append(SOLAR)
 #
 	#station_properties()
 	#print(battery_cap, " ", power)
-	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	time += delta
 	
-	pow_consumption = modules_upkeep + humans*humans_energy_upkeep
+	pow_consumption = power_construction + modules_upkeep + humans*humans_energy_upkeep
 	power = solar_power(solar_power_installed, time) - pow_consumption
 	
 	energy += power*delta
 	energy = min(energy,battery_cap)
 	
-	matter_transf = - humans*humans_matter_upkeep
-	matter += matter_transf*delta/1000
+	matter_change = matter_transf - humans*humans_matter_upkeep
+	matter += matter_change*delta/1000
 	
 	count_matter.text = str("%10.2f" % matter)
 	count_power.text = str("%10.1f" % solar_power_installed)
 	count_energy.text = str("%10.1f" % energy) + "/" + str("%10.1f" % battery_cap)
-	count_matter_transf.text = str(matter_transf)
+	count_matter_transf.text = str(matter_change)
 	count_humans.text = str(humans) + "/" + str(humans_cap)
 	count_consumption.text = str(pow_consumption)
 	
